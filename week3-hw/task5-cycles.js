8affefc832373db592a93f05925d51a3c6ed0bb2
Kryshtal <elenkrishtal@gmail.com>
2022-11-27 21:41:44 +0200

    Add solved 1-4 tasks

---
+++ week3-hw/task5-cycles.js
@@ -0,0 +1,46 @@
+//1
+
+for (let startNum = 20; startNum <= 30; startNum += 0.5) {
+  console.log(startNum);
+}
+
+//2
+
+const DOLLAR_EXCHANGE_RATE = 27;
+
+for (let startNum = 10; startNum < 101; startNum += 10) {
+  console.log(DOLLAR_EXCHANGE_RATE * startNum);
+}
+
+//3
+
+const MAX_SQUARE_NUM = 92;
+
+for (let startNum = 1; startNum < 101; startNum++) {
+  if (startNum ** 2 > maxSquareNum) {
+    break;
+  }
+  console.log(startNum);
+}
+
+//4
+
+const NUMBER_TO_CHECK = 943;
+
+let startNum = 2;
+
+while (startNum < NUMBER_TO_CHECK) {
+  if (NUMBER_TO_CHECK % startNum === 0) {
+    console.log("Not a primary number", NUMBER_TO_CHECK);
+    break;
+  } else if (
+    NUMBER_TO_CHECK % startNum > 0 &&
+    startNum === NUMBER_TO_CHECK - 1
+  ) {
+    console.log("Is a primary number", NUMBER_TO_CHECK);
+  }
+
+  startNum++;
+}
+
+//5
