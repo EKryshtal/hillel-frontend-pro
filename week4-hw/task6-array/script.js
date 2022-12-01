2a7576e0f3e7b38b6f8289769f268bd06bd34491
M0rrowyn <elenkrishtal@gmail.com>
2022-12-01 12:36:31 +0200

    Add fix to array with sort method using function

--- week4-hw/task6-array/script.js
+++ week4-hw/task6-array/script.js
@@ -12,7 +12,13 @@ for (let index = 0; index < Infinity; index++) {
 
 alert(arr);
 
-arr.sort();
+function compareNumeric(a, b) {
+  if (a.length > b.length) return 1;
+  if (a.length === b.length) return 0;
+  if (a.length < b.length) return -1;
+}
+
+arr.sort(compareNumeric);
 alert(arr);
 
 arr.splice(1, 3);
