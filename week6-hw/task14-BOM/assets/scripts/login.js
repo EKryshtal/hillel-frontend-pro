e54addd30086170ca78d5dd74df3eab17c542b08
M0rrowyn <elenkrishtal@gmail.com>
2022-12-21 22:08:14 +0200

    Add solved 14th task

--- week6-hw/task14-BOM/assets/scripts/login.js
+++ week6-hw/task14-BOM/assets/scripts/login.js
@@ -1,2 +1,20 @@
+const emailInput = document.querySelector("#email");
+const passInput = document.querySelector("#password");
+const loader = document.querySelector("#loader");
 
+function onSumbit(event) {
+  event.preventDefault();
 
+  console.log(emailInput.value);
+  console.log(passInput.value);
+  
+  showLoader();
+
+  setTimeout(() => {
+    location = "/week6-hw/task14-BOM/my-profile-page.html";
+  }, 2000);
+}
+
+function showLoader() {
+  loader.style.display = "block";
+}
---
+++ week6-hw/task14-BOM/assets/scripts/my-profile.js
@@ -0,0 +1,6 @@
+console.log(screen.width)
+console.log(screen.height)
+
+function back() {
+  history.back()
+}
