09c5e0cbcab06ff70b2a83bdbbf6b8fdb5c4c906
Elena Kryshtal <elenkrishtal@gmail.com>
2023-02-11 15:09:07 +0200

    Review fixes

--- week12-hw/task22-custom-generator/script.js
+++ week12-hw/task22-custom-generator/script.js
@@ -1,14 +1,12 @@
-const array = [1, 2, 3];
-let startNumber = 0;
-
 function Boo() {
+  const array = [1, 2, 3];
+  let startNumber = 0;
+  
   return {
     next: function () {
-      if (startNumber < array.length) {
-        return { value: array[startNumber++], done: false };
-      } else {
-        return { value: undefined, done: true };
-      }
+      return startNumber < array.length
+        ? { value: array[startNumber++], done: false }
+        : { value: undefined, done: true };
     },
   };
 }
